233d14b4c0ac3edaf04214b5f33bf3a97f513595
tren-parasoft <109122853+tren-parasoft@users.noreply.github.com>
2026-05-20 10:44:27 +0800

    Update comment for SERVER_IP constant

--- src/main/java/com/socket/SocketConstant.java
+++ src/main/java/com/socket/SocketConstant.java
@@ -4,6 +4,6 @@ public class SocketConstant {
     // Port number
     public static final Integer TCP_PORT = 12000;
 
-    // Server IP
+    // Server IP example
     public static final String SERVER_IP = "192.168.3.32";
 }
